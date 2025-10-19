568944e1032b124b308ead685015c8f5196e089a
Arun pandian <arunpandian972000@gmail.com>
2025-10-19 13:52:07 +0530

    deploy v-7

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic";
+import React from 'react'
+import ProductPage from './product';
+
+const page = () => {
+  return (
+    <ProductPage/>
+  )
+}
+
+export default page
